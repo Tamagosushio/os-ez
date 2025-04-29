18e16c32d87d9f967a40425a3a4836559b5f8c3d
tamagosushio <mitachi.ts75@gmail.com>
2025-04-29 23:00:18 +0900

    [add] 5章Hello World

--- kernel.c
+++ kernel.c
@@ -1,3 +1,6 @@
+#include "kernel.h"
+#include "common.h"
+
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
@@ -6,6 +9,32 @@ typedef uint32_t size_t;
 // 領域の先頭アドレスを返すように配列
 extern char __bss[], __bss_end[], __stack_top[];
 
+// OpenSBIの呼び出し
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid){
+  // 指定レジスタに値を入れる
+  register long a0 __asm__("a0") = arg0;
+  register long a1 __asm__("a1") = arg1;
+  register long a2 __asm__("a2") = arg2;
+  register long a3 __asm__("a3") = arg3;
+  register long a4 __asm__("a4") = arg4;
+  register long a5 __asm__("a5") = arg5;
+  register long a6 __asm__("a6") = fid;
+  register long a7 __asm__("a7") = eid;
+  // ecall命令 -> CPUの実行モードをカーネル用からOpenSBI用に切り替わり、OpenSBIの処理ハンドラが呼び出される
+  __asm__ __volatile__(
+    "ecall"
+    : "=r"(a0), "=r"(a1)
+    : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
+    : "memory"
+  );
+  return (struct sbiret){.error = a0, .value = a1};
+}
+
+void putchar(char ch){
+  // long sbi_console_putchar(int ch) 機能を使用
+  sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
+}
+
 void *memset(void *buf, char c, size_t n){
   uint8_t *p = (uint8_t *)buf;
   while(n--) *p++ = c;
@@ -13,9 +42,16 @@ void *memset(void *buf, char c, size_t n){
 }
 
 void kernel_main(void){
-  // .bss領域を0で初期化する
-  memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
-  for(;;);
+  // // .bss領域を0で初期化する
+  // memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
+  // for(;;);
+  // const char *s = "\n\nHello world!\n";
+  // for(int i = 0; s[i] != '\0'; i++){
+  //   putchar(s[i]);
+  // }
+  printf("\n\nHello %s\n", "world!");
+  printf("1 + 2 = %d, %x", 1+2, 0x1234abcd);
+  for(;;) __asm__ __volatile__("wfi");
 }
 
 __attribute__((section(".text.boot"))) // .text.bootセクションに配置
--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
 # カーネルをビルド
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-  kernel.c
+  kernel.c common.c
 # -machine virt -> 仮想マシンとして起動
 # -bios default -> デフォルトBIOS(OpenSBI)を使用
 # -nographic -> QEMUをウィンドウなしで起動
