49a22ff30708880b9088bdbd8070b9bc2047454a
tamagosushio <mitachi.ts75@gmail.com>
2025-04-30 21:01:17 +0900

    [hotfix] typo修正

--- common.h
+++ common.h
@@ -1,10 +1,10 @@
 #pragma once
 
 typedef int bool;
-typedef unsinged char uint8_t;
-typedef unsinged short uint16_t;
-typedef unsinged int uint32_t;
-typedef unsinged long long uint64_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
 typedef uint32_t size_t;
 typedef uint32_t paddr_t; // 物理メモリアドレスを表す型
 typedef uint32_t vaddr_t; // 仮想メモリアドレスを表す型
@@ -22,6 +22,6 @@ typedef uint32_t vaddr_t; // 仮想メモリアドレスを表す型
 
 void *memset(void *buf, char c, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
-void *strcpy(char *dst, const char *src);
+char *strcpy(char *dst, const char *src);
 int strcmp(const char *s1, const char *s2);
 void printf(const char *fmt, ...);
