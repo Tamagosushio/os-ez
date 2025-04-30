bd69cc7faa929661e4110f1ad460517b83370506
tamagosushio <mitachi.ts75@gmail.com>
2025-04-30 20:42:54 +0900

    [add] 6章C標準ライブラリ

--- common.c
+++ common.c
@@ -55,3 +55,35 @@ void printf(const char *fmt, ...){
 end:
   va_end(vargs);
 }
+
+// srcからnバイトをdstにコピー
+void *memcpy(void *dst, const void *src, size_t n){
+  uint8_t *d = (uint8_t *)dst;
+  const uint8_t *s = (const uint8_t *)src;
+  while(n--) *d++ = *s++;
+  return dst;
+}
+// bufの先頭からnバイトをcで埋める
+void *memset(void *buf, char c, size_t n){
+  uint8_t *p = (uint8_t *)buf;
+  while(n--) *p++ = c;
+  return buf;
+}
+// srcの文字列をdstにコピー
+char *strcpy(char *dst, const char *src){
+  char *d = dst;
+  while(*src) *dst++ = *src++;
+  *d = '\0';
+  return dst;
+}
+// 文字列s1と文字列s2の比較
+// s1とs2が等しいとき0、s1が大きいとき正、小さいとき負の値を返す
+int strcmp(const char *s1, const char *s2){
+  while(*s1 && *s2){
+    if(*s1 != *s2) break;
+    s1++; s2++;
+  }
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
+
+
--- common.h
+++ common.h
@@ -1,8 +1,27 @@
 #pragma once
 
+typedef int bool;
+typedef unsinged char uint8_t;
+typedef unsinged short uint16_t;
+typedef unsinged int uint32_t;
+typedef unsinged long long uint64_t;
+typedef uint32_t size_t;
+typedef uint32_t paddr_t; // 物理メモリアドレスを表す型
+typedef uint32_t vaddr_t; // 仮想メモリアドレスを表す型
+
+#define true 1
+#define false 0
+#define NULL ((void *)0)
+#define align_up(value, align) __buildin_align_up(value, align) // valueをalign(2のべき乗)に切り上げ
+#define is_aligned(value, align) __buildin_is_aligned(vlaue, align) // valueがalign(2のべき乗)の倍数か判定
+#define offsetof(type, member) __buildin_offsetof(type, member) // メンバが構造体の先頭から何バイト目にあるかを返す
 #define va_list  __builtin_va_list
 #define va_start __builtin_va_start
 #define va_end   __builtin_va_end
 #define va_arg   __builtin_va_arg
 
+void *memset(void *buf, char c, size_t n);
+void *memcpy(void *dst, const void *src, size_t n);
+void *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
 void printf(const char *fmt, ...);
